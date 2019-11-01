863eb7fa556c680ccb5f762275295fa330426fe9
John Venditti <jdv005@bucknell.edu>
2019-11-01 12:21:53 -0400

    Make sure everything is up to date

--- src/comps/block.js
+++ src/comps/block.js
@@ -9,8 +9,10 @@ class Block extends Component{
     this.state={
       color: 'grey',
       width: 20,
+      checked: false,
       gameOver:false
     }
+    this.checkSurrounding();
   }
 
   checkTopLeft(){
@@ -89,7 +91,7 @@ class Block extends Component{
       return counter;
     }
     //Top Row w/o Corners
-    if(index > 0 && index < this.props.boardWidth){
+    else if(index > 0 && index < this.props.boardWidth){
       counter = counter+this.checkMiddleRight();
       counter = counter+this.checkBottomRight();
       counter = counter+this.checkBottomMiddle();
@@ -98,14 +100,14 @@ class Block extends Component{
       return counter;
     }
     //Top Right Corner
-    if(index === this.props.boardWidth-1){
+    else if(index === this.props.boardWidth-1){
       counter = counter+this.checkMiddleLeft();
       counter = counter+this.checkBottomLeft();
       counter = counter+this.checkBottomMiddle();
       return counter;
     }
     //Left Side
-    if(index%this.props.boardWidth === 0 && (index !==0 || index !==(this.props.board.length-this.props.boardWidth))){
+    else if(index%this.props.boardWidth === 0 && (index !==0 && index !==(this.props.board.length-this.props.boardWidth))){
       counter = counter+this.checkMiddleRight();
       counter = counter+this.checkBottomRight();
       counter = counter+this.checkBottomMiddle();
@@ -114,7 +116,7 @@ class Block extends Component{
       return counter;
     }
     //Right Side
-    if(index%this.props.boardWidth === 21 && (index !== (this.props.boardWidth-1)||index !== (this.props.board.length-1))){
+    else if(index%this.props.boardWidth === 21 && (index !== (this.props.boardWidth-1) && index !== (this.props.board.length-1))){
       counter = counter+this.checkMiddleLeft();
       counter = counter+this.checkBottomLeft();
       counter = counter+this.checkBottomMiddle();
@@ -123,12 +125,26 @@ class Block extends Component{
       return counter;
     }
     //Bottom Row w/o Corners
-    if(index > (this.props.board.length-this.props.boardWidth) && index < this.props.board.length){
+    else if(index > (this.props.board.length-this.props.boardWidth) && index < this.props.board.length-1){
       counter = counter+this.checkMiddleRight();
       counter = counter+this.checkTopRight();
       counter = counter+this.checkTopMiddle();
       counter = counter+this.checkTopLeft();
       counter = counter+this.checkMiddleLeft();
+      return counter;
+    }
+    //Bottom Right Corner
+    else if(index === this.props.board.length-1){
+      counter = counter+this.checkMiddleLeft();
+      counter = counter+this.checkTopLeft();
+      counter = counter+this.checkTopMiddle();
+      return counter;
+    }
+    //Bottom Left Corner
+    else if(index === this.props.board.length-this.props.boardWidth){
+      counter = counter+this.checkMiddleRight();
+      counter = counter+this.checkTopRight();
+      counter = counter+this.checkTopMiddle();
       return counter;
     }
     //All middle spaces
@@ -145,60 +161,149 @@ class Block extends Component{
     }
   }
 
+  noBombs(){
+    var index = this.props.index;
+    var width = this.props.boardWidth;
+//Top Left Corner
+    if(index === 0){
+      if(this.props.board[index+1]!=='2'){this.props.openWhites(index+1);}
+      if(this.props.board[index+(width)]!=='2'){this.props.openWhites(index+(width));}
+      if(this.props.board[index+(width+1)]!=='2'){this.props.openWhites(index+(width+1));}
+    }
+//Top Row
+    else if(index > 0 && index < this.props.boardWidth){
+      if(this.props.board[index+1]!=='2'){this.props.openWhites(index+1);}
+      if(this.props.board[index-1]!=='2'){this.props.openWhites(index-1);}
+      if(this.props.board[index+(width-1)]!=='2'){this.props.openWhites(index+(width-1));}
+      if(this.props.board[index+(width)]!=='2'){this.props.openWhites(index+(width));}
+      if(this.props.board[index+(width+1)]!=='2'){this.props.openWhites(index+(width+1));}
+    }
+//Top Right Corner
+    else if(index === this.props.boardWidth-1){
+      if(this.props.board[index-1]!=='2'){this.props.openWhites(index-1);}
+      if(this.props.board[index+(width)]!=='2'){this.props.openWhites(index+(width));}
+      if(this.props.board[index+(width-1)]!=='2'){this.props.openWhites(index+(width-1));}
+    }
+//Left
+    else if(index%this.props.boardWidth === 0 && (index !==0 && index !==(this.props.board.length-this.props.boardWidth))){
+      if(this.props.board[index+1]!=='2'){this.props.openWhites(index+1);}
+      if(this.props.board[index-(width-1)]!=='2'){this.props.openWhites(index-(width-1));}
+      if(this.props.board[index-(width)]!=='2'){this.props.openWhites(index-(width));}
+      if(this.props.board[index+(width)]!=='2'){this.props.openWhites(index+(width));}
+      if(this.props.board[index+(width+1)]!=='2'){this.props.openWhites(index+(width+1));}
+    }
+//Right
+    else if(index%this.props.boardWidth === 21 && (index !== (this.props.boardWidth-1) && index !== (this.props.board.length-1))){
+      if(this.props.board[index-1]!=='2'){this.props.openWhites(index-1);}
+      if(this.props.board[index-(width+1)]!=='2'){this.props.openWhites(index-(width+1));}
+      if(this.props.board[index-(width)]!=='2'){this.props.openWhites(index-(width));}
+      if(this.props.board[index+(width)]!=='2'){this.props.openWhites(index+(width));}
+      if(this.props.board[index+(width-1)]!=='2'){this.props.openWhites(index+(width-1));}
+    }
+//Bottom Row
+    else if(index > (this.props.board.length-this.props.boardWidth) && index < this.props.board.length-1){
+      if(this.props.board[index-1]!=='2'){this.props.openWhites(index-1);}
+      if(this.props.board[index-(width+1)]!=='2'){this.props.openWhites(index-(width+1));}
+      if(this.props.board[index-(width)]!=='2'){this.props.openWhites(index-(width));}
+      if(this.props.board[index-(width-1)]!== 2){this.props.openWhites(index-(width-1));}
+      if(this.props.board[index+1] !== 2){this.props.openWhites(index+1);}
+    }
+//Bottom Right Corner
+    else if(index === this.props.board.length-1){
+      if(this.props.board[index-1]!=='2'){this.props.openWhites(index-1);}
+      if(this.props.board[index-(width)]!=='2'){this.props.openWhites(index-(width));}
+      if(this.props.board[index-(width+1)]!=='2'){this.props.openWhites(index-(width+1));}
+    }
+//Bottom Left Corner
+    else if(index === this.props.board.length-this.props.boardWidth){
+      if(this.props.board[index+1]!=='2'){this.props.openWhites(index+1);}
+      if(this.props.board[index-(width)]!=='2'){this.props.openWhites(index-(width));}
+      if(this.props.board[index-(width-1)]!=='2'){this.props.openWhites(index-(width-1));}
+    }
+//Middle
+    else{
+      console.log(this.props.board[index-1]);
+      if(this.props.board[index-1]!=='2'){this.props.openWhites(index-1);}
+      if(this.props.board[index+1]!=='2'){this.props.openWhites(index+1);}
+      if(this.props.board[index-(width+1)]!=='2'){this.props.openWhites(index-(width+1));}
+      if(this.props.board[index-(width-1)]!=='2'){this.props.openWhites(index-(width-1));}
+      if(this.props.board[index-(width)]!=='2'){this.props.openWhites(index-(width));}
+      if(this.props.board[index+(width)]!=='2'){this.props.openWhites(index+(width));}
+      if(this.props.board[index+(width-1)]!=='2'){this.props.openWhites(index+(width-1));}
+      if(this.props.board[index+(width)+1]!=='2'){this.props.openWhites(index+(width+1));}
+    }
+
+
+  }
+
 
   handleClick=e=>{
-    if (this.props.bomb === '1'){
-      e.target.to({
-      fill:'red'
-      })
 
+    if (this.props.bomb === '1'){
+      this.setState({
+      color:'red',
+      checked: true
+      })
       this.setState({
         gameOver:true
       });
     }else if(this.checkSurrounding()=== 1){
-      e.target.to({
-      fill:'green'
+      this.setState({
+      color:'green',
+      checked: true
       })
-
+      this.props.setChecked(this.props.index);
     }else if(this.checkSurrounding()=== 2){
-      e.target.to({
-      fill:'blue'
+      this.setState({
+      color:'blue',
+      checked: true
       })
-
+      this.props.setChecked(this.props.index);
     }else if(this.checkSurrounding()=== 3){
-      e.target.to({
-      fill:'yellow'
+      this.setState({
+      color:'yellow',
+      checked: true
       })
-
+      this.props.setChecked(this.props.index);
     }else if(this.checkSurrounding()=== 4){
-      e.target.to({
-      fill:'orange'
+      this.setState({
+      color:'orange',
+      checked: true
       })
-
+      this.props.setChecked(this.props.index);
     }else if(this.checkSurrounding()=== 5){
-      e.target.to({
-      fill:'black'
+      this.setState({
+      color:'black',
+      checked: true
       })
-
+      this.props.setChecked(this.props.index);
     }else if(this.checkSurrounding()=== 6){
-      e.target.to({
-      fill:'purple'
+      this.setState({
+      color:'purple',
+      checked: true
       })
-
+      this.props.setChecked(this.props.index);
     }else if(this.checkSurrounding()=== 7){
-      e.target.to({
-      fill:'teal'
+      this.setState({
+      color:'pink',
+      checked: true
       })
-
+      this.props.setChecked(this.props.index);
     }else if(this.checkSurrounding()=== 8){
-      e.target.to({
-      fill:'white'
+      this.setState({
+      color:'indigo',
+      checked: true
     })
+    this.props.setChecked(this.props.index);
     }else{
-      e.target.to({
-        fill:'white'
-      })
+      this.setState({
+      color:'white',
+      checked: true
+    });
+    //Fix this call back 
+    this.props.setChecked(this.props.index,function(){this.noBombs()});
     }
+
   }
 
   render(){
--- src/comps/screen.js
+++ src/comps/screen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Block from './block.js';
 
+
 class Screen extends Component{
 
   constructor(props){
@@ -11,10 +12,64 @@ class Screen extends Component{
       boardWidth: 22,
       numTiles:484,
       numBombs:99,
+      checked:[],
       board:[]
+    }
+    this.setBoard();
+    this.openWhites=this.openWhites.bind(this);
+    this.setChecked=this.setChecked.bind(this);
+  }
+
+  openWhites(index){
+    for( let ref in this.refs){
+      if(this.refs[ref].props.index === index){
+        console.log(index);
+        if(this.refs[ref].checkSurrounding() === 0){
+          this.refs[ref].setState({
+            color:'white'
+          });
+          this.setChecked(index);
+
+        }else if(this.refs[ref].checkSurrounding() === 1){
+          this.refs[ref].setState({
+            color:'green'
+          });
+          this.setChecked(index);
+        }else if(this.refs[ref].checkSurrounding() === 2){
+          this.refs[ref].setState({
+            color:'blue'
+          });
+          this.setChecked(index);
+        }else if(this.refs[ref].checkSurrounding() === 3){
+          this.refs[ref].setState({
+            color:'yellow'
+          });
+          this.setChecked(index);
+        }else if(this.refs[ref].checkSurrounding() === 4){
+          this.refs[ref].setState({
+            color:'orange'
+          });
+          this.setChecked(index);
+        }else if(this.refs[ref].checkSurrounding() === 5){
+          this.refs[ref].setState({
+            color:'black'
+          });
+          this.setChecked(index);
+        }
+        console.log(this.state.board[index]);
+        break;
+      }
     }
   }
 
+  setChecked(index, _callback){
+    let board = [...this.state.board];
+    let item = {...board[index]};
+    item = '2';
+    board[index] = item;
+    this.setState({board},()=>console.log(this.state.board));
+    _callback();
+  }
 
   setBoard(){
     var bombs = [];
@@ -31,21 +86,45 @@ class Screen extends Component{
       }else{this.state.board.push('0');}
 
     }
-
   }
 
+
+  //
+  // componentDidMount(){
+  //   this.setBoard();
+  //   this.setState({
+  //     tiles: this.state.board.map((pixel,index)=>{
+  //         return(
+  //           <Block
+  //           x={index%22}
+  //           y={(Math.trunc(index/22))}
+  //           index = {index}
+  //           bomb = {pixel}
+  //           board = {this.state.board}
+  //           openWhites = {this.openWhites}
+  //           boardWidth = {this.state.boardWidth}
+  //           />
+  //         )
+  //       })
+  //   })
+  //   console.log(this.state.tiles);
+  // }
+
   render(){
-    this.setBoard();
+
     return(
       this.state.board.map((pixel,index)=>{
           return(
             <Block
+            ref = {'ref'+index.toString()}
             x={index%22}
             y={(Math.trunc(index/22))}
             index = {index}
             bomb = {pixel}
             board = {this.state.board}
+            openWhites = {this.openWhites}
             boardWidth = {this.state.boardWidth}
+            setChecked = {this.setChecked}
             />
           )
         })
